4980f9bc42744f2b97332eb1e5233f5a4c8adddc
h3poteto <h3.poteto@gmail.com>
2016-04-29 15:55:52 +0900

    projectのロードをDriverを使って値の伝搬を行う

---
+++ fascia-ios/Actions/ProjectsRequest.swift
@@ -0,0 +1,42 @@
+//
+//  ProjectsRequest.swift
+//  fascia-ios
+//
+//  Created by akirafukushima on 2016/04/29.
+//  Copyright © 2016年 h3poteto. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class ProjectsRequest {
+    final let isLoading = Variable(false)
+    final let projects = Variable([Project]())
+    final let error: Variable<ErrorType?> = Variable(nil)
+    final let disposeBag = DisposeBag()
+
+    func request() {
+        if isLoading.value {
+            return
+        }
+        isLoading.value = true
+        error.value = nil
+        FasciaAPIService.sharedInstance.callBasicAPI("/projects", method: .GET, params: nil)
+            .subscribeOn(Scheduler.sharedInstance.backgroundScheduler)
+            .map({ (data, response) -> [Project] in
+                guard let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as? [[String: AnyObject]] else {
+                    fatalError("parse error")
+                }
+                return Project.buildWithArray(json)
+            })
+            .subscribe(onNext: { (projects) in
+                self.projects.value = projects
+                }, onError: { (errorType) in
+                    self.error.value = errorType
+                    self.isLoading.value = false
+                }, onCompleted: {
+                    self.isLoading.value = false
+                }, onDisposed: nil)
+            .addDisposableTo(self.disposeBag)
+    }
+}
