ab98a43a0f25668927312856e72b49ba41320b42
h3poteto <h3.poteto@gmail.com>
2016-05-16 00:52:55 +0900

    refs #23 Projects一覧画面からログインリダイレクトされた際に，戻ってきた瞬間にprojectをリロードさせる

---
+++ fascia-ios/Utils/UIViewController+Rx.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewController+Rx.swift
+//  fascia-ios
+//
+//  Created by akirafukushima on 2016/05/16.
+//  Copyright © 2016年 h3poteto. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension UIViewController {
+    private func trigger(selector: Selector) -> Observable<Void> {
+        return rx_sentMessage(selector).map { _ in () }.shareReplay(1)
+    }
+
+    var rx_viewWillAppear: Observable<Void> {
+        return trigger(#selector(self.viewWillAppear(_:)))
+    }
+
+    var rx_viewDidAppear: Observable<Void> {
+        return trigger(#selector(self.viewDidAppear(_:)))
+    }
+
+    var rx_viewWillDisappear: Observable<Void> {
+        return trigger(#selector(self.viewWillDisappear(_:)))
+    }
+
+    var rx_viewDidDisappear: Observable<Void> {
+        return trigger(#selector(self.viewDidDisappear(_:)))
+    }
+}
