5d34f69e6b70d2fa9c72677e4158580b77342bc4
h3poteto <h3.poteto@gmail.com>
2017-02-21 00:47:21 +0900

    refs #80 Add toolbar in sign in webview for share button

--- fascia-ios/ViewControllers/SignInViewController.swift
+++ fascia-ios/ViewControllers/SignInViewController.swift
@@ -18,6 +18,21 @@ class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
     var openSideMenu: UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "SideMenu"), style: UIBarButtonItemStyle.plain, target: nil, action: nil)
 
     @IBOutlet private weak var webView: UIWebView!
+    @IBOutlet private weak var shareButton: UIBarButtonItem!
+    @IBOutlet private weak var forwardButton: UIBarButtonItem!
+    @IBOutlet private weak var backButton: UIBarButtonItem!
+    @IBOutlet private weak var reloadButton: UIBarButtonItem!
+
+    struct WebViewPage {
+        var title: String?
+        var url: URL?
+        init() {
+            title = "Fascia"
+            url = URL(string: "https://fascia.io")
+        }
+    }
+
+    var pageInfo = WebViewPage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +60,8 @@ class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
     }
 
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        pageInfo.url = request.url
+        pageInfo.title = webView.stringByEvaluatingJavaScript(from: "document.title")
         if request.url?.host == URL(string: viewModel.signInURL!)?.host && request.url?.path == "/webviews/callback" {
             viewModel.update()
             viewModel.isLoading.value = false
@@ -57,5 +74,57 @@ class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
 
     private func bindViewModel() {
         hud.bind(loadingTarget: viewModel.isLoading.asDriver())
+
+        backButton
+            .rx
+            .tap
+            .subscribe(onNext: { () in
+                self.webView.goBack()
+            }, onError: nil, onCompleted: nil)
+            .addDisposableTo(disposeBag)
+
+        forwardButton
+            .rx
+            .tap
+            .subscribe(onNext: { () in
+                self.webView.goForward()
+            }, onError: nil, onCompleted: nil)
+            .addDisposableTo(disposeBag)
+
+        reloadButton
+            .rx
+            .tap
+            .subscribe(onNext: { () in
+                self.webView.reload()
+            }, onError: nil, onCompleted: nil)
+            .addDisposableTo(disposeBag)
+
+        shareButton
+            .rx
+            .tap
+            .subscribe(onNext: { () in
+                guard let shareText = self.pageInfo.title else {
+                    return
+                }
+                guard let shareURL = self.pageInfo.url else {
+                    return
+                }
+                let activityItems = [shareText, shareURL] as [Any]
+                let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+
+                let excludedActivityTypes = [
+                    UIActivityType.postToFacebook,
+                    UIActivityType.postToTwitter,
+                    UIActivityType.message,
+                    UIActivityType.saveToCameraRoll,
+                    UIActivityType.print
+                ]
+
+                activityVC.excludedActivityTypes = excludedActivityTypes
+
+                // UIActivityViewControllerを表示
+                self.present(activityVC, animated: true, completion: nil)
+            }, onError: nil, onCompleted: nil)
+            .addDisposableTo(disposeBag)
     }
 }
