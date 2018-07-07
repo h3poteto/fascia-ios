b990b2fabb527633b1b5b7a03e5f7becdf0182b1
AkiraFukushima <h3.poteto@gmail.com>
2018-07-07 17:44:01 +0900

    refs #108 Use WKWebView instead of UIWebView

--- fascia-ios/ViewControllers/SignInViewController.swift
+++ fascia-ios/ViewControllers/SignInViewController.swift
@@ -10,24 +10,23 @@ import UIKit
 import SideMenu
 import RxSwift
 import RxCocoa
+import WebKit
 
-class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
+class SignInViewController: UIViewController, WKNavigationDelegate, SideMenuable {
     private let viewModel = SignInViewModel()
     private var hud = HUDManager()
     var disposeBag = DisposeBag()
     var openSideMenu: UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "SideMenu"), style: UIBarButtonItemStyle.plain, target: nil, action: nil)
 
-    @IBOutlet private weak var webView: UIWebView!
+    @IBOutlet private weak var webView: WKWebView!
     @IBOutlet private weak var shareButton: UIBarButtonItem!
     @IBOutlet private weak var forwardButton: UIBarButtonItem!
     @IBOutlet private weak var backButton: UIBarButtonItem!
     @IBOutlet private weak var reloadButton: UIBarButtonItem!
 
     struct WebViewPage {
-        var title: String?
         var url: URL?
         init() {
-            title = "Fascia"
             url = URL(string: "https://fascia.io")
         }
     }
@@ -37,13 +36,14 @@ class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
     override func viewDidLoad() {
         super.viewDidLoad()
         sideMenuSetup(parentController: self)
+        webView.navigationDelegate = self
         bindViewModel()
         guard let signInURL = viewModel.signInURL else {
             return
         }
         let url = URL(string: signInURL)
         let request = URLRequest(url: url!)
-        webView.loadRequest(request)
+        webView.load(request)
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,25 +51,26 @@ class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
         // Dispose of any resources that can be recreated.
     }
 
-    func webViewDidStartLoad(_ webView: UIWebView) {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         viewModel.isLoading.value = true
     }
 
-    func webViewDidFinishLoad(_ webView: UIWebView) {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         viewModel.isLoading.value = false
     }
 
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        pageInfo.url = request.url
-        pageInfo.title = webView.stringByEvaluatingJavaScript(from: "document.title")
-        if request.url?.host == URL(string: viewModel.signInURL!)?.host && request.url?.path == "/webviews/callback" {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        pageInfo.url = webView.url
+
+        if webView.url?.host == URL(string: viewModel.signInURL!)?.host && webView.url?.path == "/webviews/callback" {
             viewModel.update()
             viewModel.isLoading.value = false
             self.dismiss(animated: true, completion: nil)
             self.navigationController?.popViewController(animated: true)
-            return false
+            decisionHandler(WKNavigationActionPolicy.cancel)
+            return
         }
-        return true
+        decisionHandler(WKNavigationActionPolicy.allow)
     }
 
     private func bindViewModel() {
@@ -103,13 +104,10 @@ class SignInViewController: UIViewController, UIWebViewDelegate, SideMenuable {
             .rx
             .tap
             .subscribe(onNext: { () in
-                guard let shareText = self.pageInfo.title else {
-                    return
-                }
                 guard let shareURL = self.pageInfo.url else {
                     return
                 }
-                let activityItems = [shareText, shareURL] as [Any]
+                let activityItems = [shareURL] as [Any]
                 let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
 
                 let excludedActivityTypes = [
